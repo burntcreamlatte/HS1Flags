7e64d5ae0ae129555562bf9818d8d02463039739
Aaron Shackelford <burntcreamlatte@icloud.com>
2020-06-30 20:42:43 -0700

    Initial Commit

---
+++ HS1Flags/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HS1Flags
+//
+//  Created by Aaron Shackelford on 6/30/20.
+//  Copyright © 2020 Aaron Shackelford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
